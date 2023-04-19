a5918632ebdfb4593144e935e5f91b60110ebb57
AlanBolvan <batyroff.a@icloud.com>
2023-04-20 00:32:02 +0300

    20.04.2023 00:31
    -ландшафт
    -партиклы заклинаний

--- custom/custom_editor_icon.py
+++ custom/custom_editor_icon.py
@@ -1,4 +1,5 @@
 # -*- encoding:utf-8 -*-
 _reload_all = True
 
-data = {}
+
+data = {134230172: {'path': 'ui/134230172', 'describe': '', 'name': u'ActionBar_Background', 'key': 134230172, 'uid': '134230172', 'size': '1024X190'}}
