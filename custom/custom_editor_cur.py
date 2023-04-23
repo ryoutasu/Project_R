f0a9ea54b8e53a8f35fd4086edd514de3568d852
AlanBolvan <batyroff.a@icloud.com>
2023-04-24 01:05:36 +0300

    update 24.04.2023 01:05
    -изменил партиклы скиллов
    -добавил аудио-фон

--- custom/custom_editor_cur.py
+++ custom/custom_editor_cur.py
@@ -1,4 +1,5 @@
 # -*- encoding:utf-8 -*-
 _reload_all = True
 
-data = {}
+
+data = {134263564: {'describe': '', 'name': u'cursor', 'key': 134263564, 'uid': 'editor_cur_134263564', 'path': u'134263564.cur', 'is_custom': True}}
--- custom/custom_editor_sound.py
+++ custom/custom_editor_sound.py
@@ -1,4 +1,5 @@
 # -*- encoding:utf-8 -*-
 _reload_all = True
 
-data = {}
+
+data = {134260847: {'SoundPath': '134260847', 'describe': '', 'name': u'diablo_2_11. Tombs', 'ImportantSound': False, 'DSPId': 0, 'key': 134260847, 'Format': u'mp3', 'uid': 134260847, 'IsCustom': True, 'source': 'code_Source_Import'}}
