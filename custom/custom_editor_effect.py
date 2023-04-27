00ccd8debae9b41f4f5fc251bca0cb9e351b2790
AlanBolvan <batyroff.a@icloud.com>
2023-04-28 00:41:31 +0300

    update 28.04.2023 00:41
    -изменил анимацию рывка
    -сделал ландшафт

--- custom/custom_editor_effect.py
+++ custom/custom_editor_effect.py
@@ -1,4 +1,5 @@
 # -*- encoding:utf-8 -*-
 _reload_all = True
 
-data = {}
+
+data = {134240226: {'model_id': 134257586, 'preview_icon': '', 'name': '134240226', 'describe': 134240226, 'key': 134240226, 'res_identify': '', 'scale': 1.0, 'uid': '134240226'}}
