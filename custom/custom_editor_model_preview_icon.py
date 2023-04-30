57c90b5b507cea1584f591b8f58af28e3242aa73
AlanBolvan <batyroff.a@icloud.com>
2023-05-01 01:56:03 +0300

    update 01.05.2023 01:55
    -делаю комнаты (добавил юнити ассеты)

--- custom/custom_editor_model_preview_icon.py
+++ custom/custom_editor_model_preview_icon.py
@@ -2,4 +2,4 @@
 _reload_all = True
 
 
-data = {}
+data = {134241427: {'path': 'UP1ModelPreview/134241427', 'key': 134241427, 'size': '180X100'}, 134266742: {'path': 'UP1ModelPreview/134266742', 'key': 134266742, 'size': '180X100'}, 134224670: {'path': 'UP1ModelPreview/134224670', 'key': 134224670, 'size': '180X100'}, 134227080: {'path': 'UP1ModelPreview/134227080', 'key': 134227080, 'size': '180X100'}, 134237667: {'path': 'UP1ModelPreview/134237667', 'key': 134237667, 'size': '180X100'}, 134280032: {'path': 'UP1ModelPreview/134280032', 'key': 134280032, 'size': '180X100'}}
