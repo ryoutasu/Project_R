dd00d5833d060f5293c471fe991b1b94616397ef
AlanBolvan <batyroff.a@icloud.com>
2023-05-14 11:59:42 +0300

    update 14.05.2023 11:59
    -изменил размер карты, добавив тренировочную зону

--- custom/custom_editor_icon.py
+++ custom/custom_editor_icon.py
@@ -2,4 +2,4 @@
 _reload_all = True
 
 
-data = {134230172: {'path': 'ui/134230172', 'describe': '', 'name': u'ActionBar_Background', 'key': 134230172, 'uid': '134230172', 'size': '1024X190'}, 134238676: {'path': 'ui/134238676', 'describe': '', 'name': u'wallhaven-k796z7', 'key': 134238676, 'uid': '134238676', 'size': '1920X886'}, 134258981: {'path': 'ui/134258981', 'describe': '', 'name': u'LoadingBar_Background', 'key': 134258981, 'uid': '134258981', 'size': '512X30'}}
+data = {134230172: {'path': 'ui/134230172', 'describe': '', 'name': u'ActionBar_Background', 'key': 134230172, 'uid': '134230172', 'size': '1024X190'}, 134238676: {'path': 'ui/134238676', 'describe': '', 'name': u'wallhaven-k796z7', 'key': 134238676, 'uid': '134238676', 'size': '1920X886'}, 134258981: {'path': 'ui/134258981', 'describe': '', 'name': u'LoadingBar_Background', 'key': 134258981, 'uid': '134258981', 'size': '512X30'}, 134266120: {'path': 'ui/134266120', 'describe': '', 'name': u'Toggle_Background', 'key': 134266120, 'uid': '134266120', 'size': '86X83'}, 134248439: {'path': 'ui/134248439', 'describe': '', 'name': u'SpellSlot_Border', 'key': 134248439, 'uid': '134248439', 'size': '162X162'}}
